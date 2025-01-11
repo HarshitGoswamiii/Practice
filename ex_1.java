141bfc59877438586e619bff7b19faa31e1e3cb0
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-11 17:32:05 +0530

    commit

--- ex_1.java
+++ ex_1.java
@@ -5,11 +5,20 @@ class exer1 {
         System.out.println("CGPA : " + Percentage / 9.5);
         System.out.println("Percentage : " + Percentage);
     }
+    
+    public void divide(int dividend,int divisor){
+       int quotient = dividend/divisor;
+       int reminder = dividend%divisor;
+
+       System.out.println("Quotient : "+quotient);
+       System.out.println("Reminder : "+reminder);
+    }
 }
 
 public class ex_1 {
     public static void main(String[] args) {
         exer1 TAG = new exer1();
-        TAG.cgpa(44, 45, 39);
+        // TAG.cgpa(44, 45, 39);
+        TAG.divide(25, 4);
     }
 }
