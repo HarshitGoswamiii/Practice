2b8b48439c2ac007f48b16b413d5aad3e1954709
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-14 18:26:37 +0530

    commit

--- ex_2.java
+++ ex_2.java
@@ -1,11 +1,13 @@
-class practice{
-    public void encryption(int grade){
-        
+class practice {
+    public void encryption(char grade) {
+        System.out.println(grade++);
     }
 }
 
 class ex_2 {
     public static void main(String[] args) {
+        practice prc = new practice();
 
+        prc.encryption('a');
     }
 }
