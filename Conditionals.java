41c4356f1eaf30f69990326301a31961cbcf2e33
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-17 15:53:01 +0530

    commit

--- Conditionals.java
+++ Conditionals.java
@@ -1,7 +1,10 @@
+
 class Grade {
 
     public void passing_marks(int subject_1, int subject_2, int subject_3) {
         double result = ((subject_1 + subject_2 + subject_3) / 300.0) * 100;
+        String Formatted = String.format("%.2f", result);
+        System.out.println("Percentage : " + Formatted + "%");
         if (result >= 33) {
             System.out.println("Pass");
         } else {
@@ -14,6 +17,6 @@ class Grade {
 public class Conditionals {
     public static void main(String[] args) {
         Grade hello = new Grade();
-        hello.passing_marks(43, 54, 43);
+        hello.passing_marks(43, 54, 48);
     }
 }
