0ba5691f9d409d813d7bde6b92d1aed029900d82
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-17 16:17:03 +0530

    commit

--- Conditionals.java
+++ Conditionals.java
@@ -15,9 +15,31 @@ class Grade {
     }
 }
 
+class Tax {
+    public void income_tax(int income) {
+        int tax, tax2, tax3, total_tax = 0;
+        System.out.println("Income : " + income);
+        if (income < 249999) {
+            System.out.println("No Tax !");
+        } else if (income > 250000 || income < 499999) {
+            tax = income * 5 / 100;
+            total_tax = total_tax + tax;
+        } else if (income > 500000 || income < 999999) {
+            tax2 = income * 20 / 100;
+            total_tax = total_tax + tax2;
+        } else if (income < 1000000) {
+            tax3 = income * 30 / 100;
+            total_tax = total_tax + tax3;
+        }
+
+        System.out.println("Total Ammount Payable : " + income + total_tax);
+    }
+}
+
 public class Conditionals {
     public static void main(String[] args) {
-        Grade hello = new Grade();
-        hello.passing_marks(43, 54, 48);
+
+        // Grade hello = new Grade();
+        // hello.passing_marks(43, 54, 48);
     }
 }
