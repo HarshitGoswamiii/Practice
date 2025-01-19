b2038cbe027341f25fbbd50c13e0f752bdd1bd44
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-19 15:33:09 +0530

    commit

---
+++ looping.java
@@ -0,0 +1,13 @@
+class loops {
+    public void while_looping() {
+        int a = 10;
+        while (a >= 10) {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
+
+public class looping {
+
+}
