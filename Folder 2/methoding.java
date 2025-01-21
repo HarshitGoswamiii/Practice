fd80f662b4bc3dad14499a14c2aff556d503453e
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-21 19:02:06 +0530

    commit

--- Folder 2/methoding.java
+++ Folder 2/methoding.java
@@ -1,3 +1,11 @@
 public class methoding {
-    
+    int mysum(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+        methoding obj = new methoding();
+        obj.mysum(2, 3);
+    }
 }
