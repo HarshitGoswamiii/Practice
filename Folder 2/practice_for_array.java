d9a42ddc821bcdd5a646a866816619179187a305
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-21 14:42:33 +0530

    commit

--- Folder 2/practice_for_array.java
+++ Folder 2/practice_for_array.java
@@ -2,10 +2,10 @@ class arrayss {
     public void practice() {
         int[] arr1 = { 10, 23, 13, 32, 33 };
         // for (int i = arr1.length - 1; i >= 0; i--) {
-        // System.out.print(arr1[i] + ", ");
+        //     System.out.print(arr1[i] + ", ");
         // }
-        for (int elemnt : arr1) {
-            System.out.println(arr1[elemnt]);
+        for(int element:arr1){
+            System.out.print(element+", ");
         }
     }
 }
