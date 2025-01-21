78abb4f982a87c38b8cbb13ae52fd9a771b4a31b
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-21 19:46:17 +0530

    commit

--- Folder 2/methoding.java
+++ Folder 2/methoding.java
@@ -8,12 +8,23 @@ public class methoding {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + ", ");
         }
+
     }
 
+    static void sum(int... arr) {
+        int result = 0;
+        for (int element : arr) {
+            result += element;
+        }
+        System.out.println(result);
+    }
+
+    // <---------------------------------------------MainMethods--------------------------------------------------->
     public static void main(String[] args) {
         methoding obj = new methoding();
         // System.out.println(obj.mysum(98, 31));
-        int[] arr1 = { 23, 43, 2, 11, 43 };
-        obj.mysum(arr1);
+        // int[] arr1 = { 23, 43, 2, 11, 43 };
+        // obj.mysum(arr1);
+        obj.sum(21, 32);
     }
 }
