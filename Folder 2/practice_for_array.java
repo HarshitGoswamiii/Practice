ec203c2105200862981c6265ececd21cb0c66d66
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-21 15:06:13 +0530

    commit

--- Folder 2/practice_for_array.java
+++ Folder 2/practice_for_array.java
@@ -17,12 +17,22 @@ class arrayss {
         arr2[1][1][1] = 243;
         for (int i = 0; i < arr2.length; i++) {
             for (int j = 0; j < arr2[i].length; j++) {
-                for(int k=0;k<arr2[j].length;k++){
+                for (int k = 0; k < arr2[j].length; k++) {
                     System.out.print(arr2[i][j][k] + " ");
                 }
             }
             System.out.println();
         }
+    }
+
+    // Question 1
+    public void question_1() {
+        int sum = 0;
+        float[] arrey1 = { 2.4f, 8.3f, 3.5f, 9.6f, 9.3f };
+        for (int i = 0; i < arrey1.length; i++) {
+            sum += arrey1[i];
+        }
+        System.out.println(sum);
     }
 }
 
@@ -30,6 +40,7 @@ public class practice_for_array {
     public static void main(String[] args) {
         arrayss prc = new arrayss();
         // prc.practice();
-        prc.md_arrays();
+        // prc.md_arrays();
+        prc.question_1();
     }
 }
