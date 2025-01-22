73ce3d2abafc3e50dc69ba08b166914447080130
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-22 15:55:40 +0530

    commit

--- Folder 2/practice_set.java
+++ Folder 2/practice_set.java
@@ -2,9 +2,13 @@ public class practice_set {
 
     // Question 1-->
     static void table(int number){
-        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i*number);
+        }
     }
     public static void main(String[] args) {
 
+        practice_set prt = new practice_set();
+        prt.table(8);
     }
 }
