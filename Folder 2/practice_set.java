bd7cc06ba4dba1406364731c3cb934ed0cf5098a
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-23 16:07:09 +0530

    commit

--- Folder 2/practice_set.java
+++ Folder 2/practice_set.java
@@ -9,8 +9,8 @@ public class practice_set {
 
     // Question 2-->
     static void pattern(){
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
