9bbe47e17e9d7f41e5c83d05231e25dc8fd8e871
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-23 18:34:09 +0530

    commit

--- Folder 2/practice_set.java
+++ Folder 2/practice_set.java
@@ -60,7 +60,7 @@ public class practice_set {
     static void average(int... numbers) {
         int sum = 0;
         for (int i = 0; i <= numbers.length; i++) {
-
+            sum += numbers[i];
         }
     }
 
