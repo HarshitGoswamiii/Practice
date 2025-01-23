029b4512b6eddc6dc10e959ff80613290e254c2e
Harshit Goswami <harshitgoswami244@gmail.com>
2025-01-23 16:14:10 +0530

    commit

--- Folder 2/practice_set.java
+++ Folder 2/practice_set.java
@@ -1,14 +1,14 @@
 public class practice_set {
 
     // Question 1-->
-    static void table(int number){
+    static void table(int number) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i*number);
+            System.out.println(i * number);
         }
     }
 
     // Question 2-->
-    static void pattern(){
+    static void pattern() {
         for (int i = 0; i <= 5; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("* ");
@@ -16,10 +16,21 @@ public class practice_set {
             System.out.println();
         }
     }
+
+    // Question 3
+    static int sum_of_n(int a) {
+        if (a <= 0) {
+            return 0;
+        }
+        int sum = a + sum_of_n(a - 1);
+        return sum;
+    }
+
     public static void main(String[] args) {
 
         practice_set prt = new practice_set();
         // prt.table(8);
-        prt.pattern();
+        // prt.pattern();
+        prt.sum_of_n(5)
     }
 }
