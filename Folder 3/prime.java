5d3115a47d1e97cf68d6c2e6537b69d08d7db59d
Harshit Goswami <harshitgoswami244@gmail.com>
2025-02-01 21:05:00 +0530

    commit

--- Folder 3/prime.java
+++ Folder 3/prime.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class prime {
     static public void main(String[] args) {
+        System.out.print("Enter Number ");
+        Scanner number = new Scanner(System.in);
+        int num = number.nextInt();
+        if (isPrime(number)) {
+            System.out.println(number + " is Prime!");
+        } else {
+            System.out.println(number + " is not Prime !");
+        }
 
+    }
+
+    public boolean isPrime(int numb) {
+        for (int i = 2; i < numb / 2; i++) {
+            if (numb % i == 0) {
+
+                return false;
+            }
+        }
+        return true;
     }
 }
