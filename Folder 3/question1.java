bd571b64f7f2df7cc6281e40bb9920ead559831b
Harshit Goswami <harshitgoswami244@gmail.com>
2025-02-10 21:45:06 +0530

    commit

--- Folder 3/question1.java
+++ Folder 3/question1.java
@@ -3,8 +3,9 @@ public class question1 {
         int n = 5;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= n; j++) {
-
+                System.out.print("* ");
             }
+            System.out.println();
         }
     }
 }
