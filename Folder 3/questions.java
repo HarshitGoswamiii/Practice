20052550a5b875b62e3d3381011fcea7f0042487
Harshit Goswami <harshitgoswami244@gmail.com>
2025-02-18 21:51:34 +0530

    commit

--- Folder 3/questions.java
+++ Folder 3/questions.java
@@ -1,11 +1,21 @@
-class examples{
-    public void palindrome(int number){
-        
+class examples {
+    public void palindrome_str(String name) {
+        String Reverse_Str = "";
+        int str_length = name.length();
+        for (int i = (str_length - 1); i >= 0; --i) {
+            Reverse_Str = Reverse_Str + name.charAt(i);
+        }
+        if (name.toLowerCase() == Reverse_Str.toLowerCase()) {
+            System.out.println("Palindrome String");
+        } else {
+            System.out.println("Not A Palindrome String");
+        }
     }
 }
 
 public class questions {
     public static void main(String[] args) {
-        
+        examples hello = new examples();
+        hello.palindrome_str("Radar");
     }
 }
